3ecdfc2797bb12b6b27899ec15d5062d2d7cc627
Stephen Birarda <commit@birarda.com>
2012-12-06 10:43:43 -0500

    add cluster buttons to the search page

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,22 @@ class SearchController < ApplicationController
     @page = params.has_key?(:page) ? params[:page].to_i : 1
     offset = (@page - 1) * 10
     @query = params[:query]
- 	  @index = Ferret::Index::Index.new(:path => 'new_index/index')
+    @cluster = params.has_key?(:cluster) ? params[:cluster].to_i : nil
+    
+    if @cluster.nil?
+ 	    @index = Ferret::Index::Index.new(:path => 'new_index/index')
+    else
+      @index = Ferret::Index::Index.new(:path => "clusters/#{@cluster}")
+    end
+
     @results = @index.search @query, {:offset => offset}
+    
+    File.open('cluster_indices', 'r') do |file|
+      @clusters = YAML::load(file.read)
+    end
+
+    File.open('doc_cluster_index', 'r') do |file|
+      @doc_cluster_index = YAML::load(file.read)
+    end
  	end
 end
